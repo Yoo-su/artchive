c9bbc6e2d7c43f45ae86a4cac43ee587fe58bd7d
suhyun <suhyun0871@news1.kr>
2026-07-07 15:16:12 +0900

    feat(web): 리뷰 작성 에디터 퀵 툴바 ux 개선

--- apps/web/src/features/review/components/review-viewer/index.tsx
+++ apps/web/src/features/review/components/review-viewer/index.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { Color } from "@tiptap/extension-color";
+import Highlight from "@tiptap/extension-highlight";
+import Link from "@tiptap/extension-link";
+import TextAlign from "@tiptap/extension-text-align";
+import { TextStyle } from "@tiptap/extension-text-style";
+import Underline from "@tiptap/extension-underline";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { useEffect } from "react";
+import ImageResize from "tiptap-extension-resize-image";
 
 interface ReviewViewerProps {
   content: string;
@@ -9,7 +17,23 @@ interface ReviewViewerProps {
 
 export const ReviewViewer = ({ content }: ReviewViewerProps) => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      Link.configure({
+        openOnClick: true,
+        autolink: true,
+      }),
+      TextStyle,
+      Color,
+      Highlight.configure({
+        multicolor: true,
+      }),
+      Underline,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      ImageResize,
+    ],
     content,
     editable: false,
     editorProps: {
@@ -19,6 +43,13 @@ export const ReviewViewer = ({ content }: ReviewViewerProps) => {
       },
     },
   });
+
+  // 비동기적인 콘텐츠 로딩에 맞춰 에디터 내용 동기화
+  useEffect(() => {
+    if (editor && content !== editor.getHTML()) {
+      editor.commands.setContent(content);
+    }
+  }, [content, editor]);
 
   if (!editor) {
     return null;
